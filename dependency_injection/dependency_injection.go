d8900f1e2224ee326e8d68641759d44ae2d445b7
Mehdi BENFREDJ <mehdibenfredj3@gmail.com>
2025-08-13 16:50:22 +0200

    dependency injection

--- dependency_injection/dependency_injection.go
+++ dependency_injection/dependency_injection.go
@@ -1,1 +1,9 @@
 package dependencyinjection
+
+import (
+	"io"
+)
+
+func Greet(w io.Writer, s string) {
+	w.Write(([]byte)("Hello, " + s))
+}
--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
 
 func main() {
-	fmt.Println(Hello("Mehdi", "French"))
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
 
 func Hello(n string, l string) string {
